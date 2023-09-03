4340fc644e7065dab5c8420c96f4965d67c61078
aaaaa ttttt <ryudai.0728@gmail.com>
2023-09-03 10:18:48 +0900

    あ

--- WinApp.h
+++ WinApp.h
@@ -8,13 +8,28 @@ public: // 静的メンバ変数
 	static const int32_t kWindowWidth = 1280; // 横
 	static const int32_t kWindowHeight = 720; // 縦
 
-public: // 静的メンバ関数
+	// ウィンドウクラス名
+	static const wchar_t kWindowClassName[];
 
-	// シングルトンインスタンス
-	static WinApp* GetInstance();
+public: // メンバ関数
 
 	// ウィンドウプロシージャ
 	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
+	// メッセージ処理
+	bool ProcessMessage();
+
+	// ウィンドウを作る
+	void CreateGameWindow(
+		const wchar_t* title = L"DirectXGame", UINT windowStyle = WS_OVERLAPPEDWINDOW,
+		int32_t clientWidth = kWindowWidth, int32_t clientHeight = kWindowHeight);
+
+private: // メンバ変数
+
+	// Window関連
+	HWND hwnd_ = nullptr;   // ウィンドウハンドル
+	WNDCLASS wc{}; // ウィンドウクラス
+	UINT windowStyle_;
+	
 };
 
