9fbb62adc2a98df615568dc3167b3e2fee3420b3
aaaaa ttttt <ryudai.0728@gmail.com>
2023-11-06 19:39:31 +0900

    main.cpp整理

--- main.cpp
+++ main.cpp
@@ -1,79 +1,11 @@
-#include "WinApp.h"
-#include "DirectXCommon.h"
-#include "ShaderCompile.h"
-#include "GameScene.h"
-#include "GraphicsPipeline.h"
-#include "ImGuiManager.h"
-#include "TextureManager.h"
-#include "Input.h"
-
+#include "Engine.h"
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-
-	// リワークチェック(できてるかわからん)
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
-	// COMの初期化
-	CoInitializeEx(0, COINIT_MULTITHREADED);
-	
-	WinApp* win = WinApp::GetInstance();
-	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-	
-	// ウィンドウの作成
-	win->CreateGameWindow(L"DirectXGame");
-	// DirectX初期化
-	dxCommon->Initialize(win);
-	Input::Initialize();
-	GraphicsPipeline::Initialize();
-	TextureManager::GetInstance()->Initialize();
-
-	// ゲームシーンの初期化
-	GameScene* gameScene = new GameScene();
-	gameScene->Initialize();
-
-	// ImGuiの初期化
-	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
-	imguiManager->Initialize(win, dxCommon);
-
-	// メインループ
-	while (true) {
-		// メッセージ処理
-		if (win->ProcessMessage()) {
-			break;
-		}
-
-		// input
-		Input::Update();
-		//imgui受付開始
-		imguiManager->Begin();
-		// 開発用のUIの処理。実際に開発用のUIを出す場合はここをゲーム固有の処理に置き換える
-		ImGui::ShowDemoWindow();
-
-		// ゲームの処理
-		gameScene->Update();
-
-		imguiManager->End();
-
-		// 描画前処理
-		dxCommon->PreDraw();
-
-		// ゲームシーン描画
-		gameScene->Draw();
-		
-		imguiManager->Draw();
-		// 描画後処理
-		dxCommon->PostDraw();
-
-	}
-
-	delete gameScene;
-	imguiManager->Finalize();
-
-	// COMの終了処理
-	CoUninitialize();
-  
-	CloseWindow(win->GetHwnd());
-  
+	Engine* engine = new Engine;
+	engine->Initialize();
+	engine->Update();
+	engine->Finalize();
+	delete engine;
 	return 0;
 }
