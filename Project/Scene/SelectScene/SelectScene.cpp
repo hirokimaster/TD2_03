8acbb465fd6ee9a1229076e6315c120dab1ef799
ニヘイリュウダイ <115063428+hirokimaster@users.noreply.github.com>
2024-01-16 11:43:48 +0900

    refs #2407 ステージ選択完成

---
+++ Project/Scene/SelectScene/SelectScene.cpp
@@ -0,0 +1,53 @@
+#include "SelectScene.h"
+
+SelectScene::SelectScene(){}
+
+SelectScene::~SelectScene(){}
+
+void SelectScene::Initialize()
+{
+	texHandleSelect_ = TextureManager::Load("resources/stageSelect.png");
+	spriteSelect_.reset(Sprite::Create({ 0,0 }, { 1280.0f,720.0f }));
+	selectNum_ = 1;
+	camera_.Initialize();
+}
+
+void SelectScene::Update()
+{
+	// ステージ番号選択
+	if (Input::GetInstance()->PressedKey(DIK_RIGHT) && selectNum_ < 3) {
+		selectNum_ += 1;
+	}
+	else if (Input::GetInstance()->PressedKey(DIK_LEFT) && selectNum_ > 1) {
+		selectNum_ -= 1;
+	}
+
+	// 番号によってステージ変更
+	if (Input::GetInstance()->PressedKey(DIK_RETURN) && selectNum_ == 1) {
+		stageNo_ = EASY;
+	}
+	else if (Input::GetInstance()->PressedKey(DIK_RETURN) && selectNum_ == 2) {
+		stageNo_ = NORMAL;
+	}
+	else if (Input::GetInstance()->PressedKey(DIK_RETURN) && selectNum_ == 3) {
+		stageNo_ = HARD;
+	}
+
+	// ゲームシーンに切り替え
+	if (Input::GetInstance()->PressedKey(DIK_RETURN)) {
+		sceneNo_ = GAME;
+	}
+
+	camera_.UpdateMatrix();
+}
+
+void SelectScene::Draw()
+{
+	spriteSelect_->Draw(camera_, texHandleSelect_);
+#ifdef _DEBUG
+	ImGui::Begin("stageSlect");
+	ImGui::Text("selectNum = %d", selectNum_);
+	ImGui::End();
+#endif // _DEBUG
+
+}
