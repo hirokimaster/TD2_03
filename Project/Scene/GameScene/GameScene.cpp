823c658e6b8fd1818b6d3bf21d575e65815b11d7
洋輝 大橋 <hiro0084169@outlook.jp>
2024-02-06 23:46:33 +0900

    ライトの調整

--- Project/Scene/GameScene/GameScene.cpp
+++ Project/Scene/GameScene/GameScene.cpp
@@ -26,11 +26,11 @@ void GameScene::Initialize() {
 	player_ = std::make_unique<Player>();
 	player_->Initialize();
 
-	pointLight_.color = { 1.0f,0.3f,0.0f,1.0f };
+	pointLight_.color = { 1.0f,0.4f,0.0f,1.0f };
 	pointLight_.position = { 0.0f,30.0f,-0.5f };
-	pointLight_.intensity = 1.0f;
-	pointLight_.radius = 160.0f;
-	pointLight_.decay = 1.2f;
+	pointLight_.intensity = 0.3f;
+	pointLight_.radius = 300.0f;
+	pointLight_.decay = 1.5f;
 
 	shakeTime = 0;
 
